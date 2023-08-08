ff86c1b19e90eb497a0e8a590032b8a5e4336e50
Dr Colin Smith <colin.smith@corfinancialgroup.com>
2023-08-08 16:32:25 +0100

    .

--- compat.java
+++ compat.java
@@ -6,6 +6,23 @@ import java.util.HashMap;
 import com.bitaplus.BitaModel.Optimisation.OptimiserController;
 
 public class compat {
+    double seek;
+
+    static double[] s2d(String key, HashMap<String, String[]> D) {
+        String[] back = D.get(key);
+        double[] backd = new double[back.length];
+        for (int i = 0; i < back.length; ++i)
+            backd[i] = Double.parseDouble(back[i]);
+        return backd;
+    }
+
+    static int[] s2i(String key, HashMap<String, String[]> D) {
+        String[] back = D.get(key);
+        int[] backd = new int[back.length];
+        for (int i = 0; i < back.length; ++i)
+            backd[i] = Integer.parseInt(back[i]);
+        return backd;
+    }
 
     static { // Need this if it is not part of class OptimiserController
         try {
@@ -29,13 +46,17 @@ public class compat {
         String file = "prob20june.log";
         if (args.length == 1)
             file = args[0];
-        String[] keys = "n nfac names m A L soft_m soft_A soft_L soft_U soft_b soft_l U alpha shortalphacost bench Q gamma initial delta buy sell qbuy qsell kappa basket longbasket downrisk downfactor shortbasket tradebuy tradesell tradenum revise costs min_holding min_trade ls full minRisk maxRisk rmin rmax round min_lot size_lot ncomp Composites value valuel npiece hpiece pgrad nabs A_abs mabs I_A Abs_U Abs_L ShortCostScale mask issues five ten forty"
+        String[] keys = "n nfac names m A L soft_m soft_A soft_L soft_U soft_b soft_l U alpha shortalphacost bench Q gamma initial delta buy sell qbuy qsell kappa basket longbasket downrisk downfactor shortbasket tradebuy tradesell tradenum revise costs min_holding min_trade ls full minRisk maxRisk rmin rmax round min_lot size_lot ncomp Composites value valuel npiece hpiece pgrad nabs A_abs mabs I_A Abs_U Abs_L ShortCostScale mask"
                 .split(" ");
         String delimiter = " ";
         String line = "";
         String lastKey = "";
+        BufferedReader br = null;
+        FileReader fff = null;
         HashMap<String, String[]> DATA = new HashMap<String, String[]>();
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+        try {
+            fff = new FileReader(file);
+            br = new BufferedReader(fff);
             while ((line = br.readLine()) != null) {
                 // System.out.println(line);
                 if (line.contains("--------"))
@@ -44,23 +65,25 @@ public class compat {
                 if (Arrays.asList(keys).contains(line)) {
                     lastKey = line;
                     line = br.readLine();
+                    if (line == null)
+                        break;
                     values = line.split(delimiter);
                     DATA.put(lastKey, values);
-                } else {// We get here is array data goes over one line or unrecognised key word
+                } else {// We get here if array data goes over one line or unrecognised key word
                     if (!lastKey.matches("names") && line.matches("^[A-Z,a-z].*")) {
                         lastKey = line;
                         continue;
                     }
                     values = line.split(delimiter);
                     String[] here = DATA.get(lastKey);
-                    if (here != null) {
+                    if (here != null) {// Add the values in line to values in lastKey
                         int newLength = values.length + here.length;
                         String[] newValues = new String[newLength];
                         System.arraycopy(here, 0, newValues, 0, here.length);
                         System.arraycopy(values, 0, newValues, here.length, values.length);
                         DATA.put(lastKey, newValues);
                     } else {
-                        DATA.put(lastKey, values);
+                        DATA.put(lastKey, values);// Parts of the data file have what look like unknown keys
                     }
                 }
             }
@@ -68,26 +91,26 @@ public class compat {
         } catch (Exception e) {
             System.out.println(lastKey + " " + line + ":" + e);
         }
-        int n = Arrays.stream(DATA.get("n")).mapToInt(Integer::parseInt).toArray()[0];
-        int m = Arrays.stream(DATA.get("m")).mapToInt(Integer::parseInt).toArray()[0];
-        int nfac = Arrays.stream(DATA.get("nfac")).mapToInt(Integer::parseInt).toArray()[0];
-        int basket = Arrays.stream(DATA.get("basket")).mapToInt(Integer::parseInt).toArray()[0];
-        int trades = Arrays.stream(DATA.get("tradenum")).mapToInt(Integer::parseInt).toArray()[0];
-        int revise = Arrays.stream(DATA.get("revise")).mapToInt(Integer::parseInt).toArray()[0];
-        int costs = Arrays.stream(DATA.get("costs")).mapToInt(Integer::parseInt).toArray()[0];
-        int ls = Arrays.stream(DATA.get("ls")).mapToInt(Integer::parseInt).toArray()[0];
-        int full = Arrays.stream(DATA.get("full")).mapToInt(Integer::parseInt).toArray()[0];
-        double[] A = Arrays.stream(DATA.get("A")).mapToDouble(Double::parseDouble).toArray();
+        int n = s2i("n", DATA)[0];
+        int m = s2i("m", DATA)[0];
+        int nfac = s2i("nfac", DATA)[0];
+        int basket = s2i("basket", DATA)[0];
+        int trades = s2i("tradenum", DATA)[0];
+        int revise = s2i("revise", DATA)[0];
+        int costs = s2i("costs", DATA)[0];
+        int ls = s2i("ls", DATA)[0];
+        int full = s2i("full", DATA)[0];
+        double[] A = s2d("A", DATA);
         double[] A_abs = null;
         try {
-            A_abs = Arrays.stream(DATA.get("A_abs")).mapToDouble(Double::parseDouble).toArray();
+            A_abs = s2d("A_abs", DATA);
         } catch (Exception jj) {
             ;
         }
         int[] I_Ai = null;
         long[] I_A = null;
         try {
-            I_Ai = Arrays.stream(DATA.get("I_A")).mapToInt(Integer::parseInt).toArray();
+            I_Ai = s2i("I_A", DATA);
             I_A = new long[I_Ai.length];
             for (int i = 0; i < I_Ai.length; ++i) {
                 I_A[i] = (long) I_Ai[i];
@@ -95,15 +118,15 @@ public class compat {
         } catch (Exception e) {
             ;
         }
-        int round = Arrays.stream(DATA.get("round")).mapToInt(Integer::parseInt).toArray()[0];
-        int ncomp = Arrays.stream(DATA.get("ncomp")).mapToInt(Integer::parseInt).toArray()[0];
-        int npiece = Arrays.stream(DATA.get("npiece")).mapToInt(Integer::parseInt).toArray()[0];
-        int longbasket = Arrays.stream(DATA.get("longbasket")).mapToInt(Integer::parseInt).toArray()[0];
-        int shortbasket = Arrays.stream(DATA.get("shortbasket")).mapToInt(Integer::parseInt).toArray()[0];
-        int tradebuy = Arrays.stream(DATA.get("tradebuy")).mapToInt(Integer::parseInt).toArray()[0];
-        int tradesell = Arrays.stream(DATA.get("tradesell")).mapToInt(Integer::parseInt).toArray()[0];
-        int nabs = Arrays.stream(DATA.get("nabs")).mapToInt(Integer::parseInt).toArray()[0];
-        int mabs = Arrays.stream(DATA.get("mabs")).mapToInt(Integer::parseInt).toArray()[0];
+        int round = s2i("round", DATA)[0];
+        int ncomp = s2i("ncomp", DATA)[0];
+        int npiece = s2i("npiece", DATA)[0];
+        int longbasket = s2i("longbasket", DATA)[0];
+        int shortbasket = s2i("shortbasket", DATA)[0];
+        int tradebuy = s2i("tradebuy", DATA)[0];
+        int tradesell = s2i("tradesell", DATA)[0];
+        int nabs = s2i("nabs", DATA)[0];
+        int mabs = s2i("mabs", DATA)[0];
         double[][] AA = new double[n][m];
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
@@ -116,81 +139,81 @@ public class compat {
                 Abs_A[i][j] = A_abs[i + j * n];
             }
         }
-        double[] L = Arrays.stream(DATA.get("L")).mapToDouble(Double::parseDouble).toArray();
-        double[] U = Arrays.stream(DATA.get("U")).mapToDouble(Double::parseDouble).toArray();
+        double[] L = s2d("L", DATA);
+        double[] U = s2d("U", DATA);
         double[] Abs_L = null;
         try {
-            Abs_L = Arrays.stream(DATA.get("Abs_L")).mapToDouble(Double::parseDouble).toArray();
+            Abs_L = s2d("Abs_L", DATA);
         } catch (Exception e) {
             ;
         }
         double[] Abs_U = null;
         try {
-            Abs_U = Arrays.stream(DATA.get("Abs_U")).mapToDouble(Double::parseDouble).toArray();
+            Abs_U = s2d("Abs_U", DATA);
         } catch (Exception e) {
             ;
         }
-        double[] initial = Arrays.stream(DATA.get("initial")).mapToDouble(Double::parseDouble).toArray();
-        double[] alpha = Arrays.stream(DATA.get("alpha")).mapToDouble(Double::parseDouble).toArray();
-        double[] bench = Arrays.stream(DATA.get("bench")).mapToDouble(Double::parseDouble).toArray();
-        double[] Q = Arrays.stream(DATA.get("Q")).mapToDouble(Double::parseDouble).toArray();
+        double[] initial = s2d("initial", DATA);
+        double[] alpha = s2d("alpha", DATA);
+        double[] bench = s2d("bench", DATA);
+        double[] Q = s2d("Q", DATA);
         double[] buy = null;
         try {
-            Arrays.stream(DATA.get("buy")).mapToDouble(Double::parseDouble).toArray();
+            buy = s2d("buy", DATA);
         } catch (Exception e) {
             // System.out.println(e);
         }
         double[] sell = null;
         try {
-            sell = Arrays.stream(DATA.get("sell")).mapToDouble(Double::parseDouble).toArray();
+            sell = s2d("sell", DATA);
         } catch (Exception e) {
             // System.out.println(e);
         }
         double[] min_lot = null;
         try {
-            min_lot = Arrays.stream(DATA.get("min_lot")).mapToDouble(Double::parseDouble).toArray();
+            min_lot = s2d("min_lot", DATA);
         } catch (Exception e) {
             // System.out.println(e);
         }
         double[] size_lot = null;
         try {
-            size_lot = Arrays.stream(DATA.get("size_lot")).mapToDouble(Double::parseDouble).toArray();
+            size_lot = s2d("size_lot", DATA);
         } catch (Exception e) {
             // System.out.println(e);
         }
         double[] Composites = null;
         try {
-            Composites = Arrays.stream(DATA.get("Composites")).mapToDouble(Double::parseDouble).toArray();
+            Composites = s2d("Composites", DATA);
         } catch (Exception e) {
             // System.out.println(e);
         }
         double[] hpiece = null;
         try {
-            hpiece = Arrays.stream(DATA.get("hpiece")).mapToDouble(Double::parseDouble).toArray();
+            hpiece = s2d("hpiece", DATA);
         } catch (Exception e) {
             // System.out.println(e);
         }
         double[] pgrad = null;
         try {
-            pgrad = Arrays.stream(DATA.get("pgrad")).mapToDouble(Double::parseDouble).toArray();
+            pgrad = s2d("pgrad", DATA);
         } catch (Exception e) {
             // System.out.println(e);
         }
         double[] mask = null;
-        double gamma = Arrays.stream(DATA.get("gamma")).mapToDouble(Double::parseDouble).toArray()[0];
-        double delta = Arrays.stream(DATA.get("delta")).mapToDouble(Double::parseDouble).toArray()[0];
-        double kappa = Arrays.stream(DATA.get("kappa")).mapToDouble(Double::parseDouble).toArray()[0];
-        double min_hold = Arrays.stream(DATA.get("min_holding")).mapToDouble(Double::parseDouble).toArray()[0];
-        double min_trade = Arrays.stream(DATA.get("min_trade")).mapToDouble(Double::parseDouble).toArray()[0];
-        double minRisk = Arrays.stream(DATA.get("minRisk")).mapToDouble(Double::parseDouble).toArray()[0];
-        double maxRisk = Arrays.stream(DATA.get("maxRisk")).mapToDouble(Double::parseDouble).toArray()[0];
-        double rmin = Arrays.stream(DATA.get("rmin")).mapToDouble(Double::parseDouble).toArray()[0];
-        double rmax = Arrays.stream(DATA.get("rmax")).mapToDouble(Double::parseDouble).toArray()[0];
-        int downrisk = Arrays.stream(DATA.get("downrisk")).mapToInt(Integer::parseInt).toArray()[0];
-        double downfactor = Arrays.stream(DATA.get("downfactor")).mapToDouble(Double::parseDouble).toArray()[0];
-        double value = Arrays.stream(DATA.get("value")).mapToDouble(Double::parseDouble).toArray()[0];
-        double valuel = Arrays.stream(DATA.get("valuel")).mapToDouble(Double::parseDouble).toArray()[0];
-        double ShortCostScale = Arrays.stream(DATA.get("ShortCostScale")).mapToDouble(Double::parseDouble).toArray()[0];
+        double gamma = s2d("gamma", DATA)[0];
+        double delta = s2d("delta", DATA)[0];
+        double kappa = s2d("kappa", DATA)[0];
+        double min_hold = s2d("min_holding", DATA)[0];
+        double min_trade = s2d("min_trade", DATA)[0];
+        double minRisk = s2d("minRisk", DATA)[0];
+        double maxRisk = s2d("maxRisk", DATA)[0];
+        double rmin = s2d("rmin", DATA)[0];
+        double rmax = s2d("rmax", DATA)[0];
+        int downrisk = s2i("downrisk", DATA)[0];
+        double downfactor = s2d("downfactor", DATA)[0];
+        double value = s2d("value", DATA)[0];
+        double valuel = s2d("valuel", DATA)[0];
+        double ShortCostScale = s2d("ShortCostScale", DATA)[0];
         double zetaF = 1;
         double zetaS = 1;
 
